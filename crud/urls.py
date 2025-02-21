5576df989eea7c25f2f74bf1c6f5fc49a9e0cf68
T-Terra <gabst138@gmail.com>
2025-02-21 18:08:58 -0300

    fix: save user duplicate and splic create user and list

--- crud/urls.py
+++ crud/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('users/', views.users, name='users'),
+    path('users/add/', views.users, name='add_user'),
+    path('users/', views.listUsers, name='list_users'),
 ]
--- crud/views.py
+++ crud/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import User
 
 # Create your views here.
@@ -6,13 +6,18 @@ from .models import User
 def index(request):
     return render(request, 'crud/form.html')
 
+#Create
 def users(request):
     new_user = User()
     new_user.name = request.POST.get('nome')
     new_user.age = request.POST.get('idade')
     new_user.save()
 
+    return redirect('list_users')
+
+#Read
+def listUsers(request):
     users_db = {
-        'users': User.objects.all()
+    'users': User.objects.all()
     }
     return render(request, 'crud/users.html', users_db)
