654b4e10227a45908a9bcc7586ea81e7e014c297
T-Terra <gabst138@gmail.com>
2025-02-22 18:32:15 -0300

    feat: add test delete user e2e

--- crud/tests.py
+++ crud/tests.py
@@ -15,10 +15,15 @@ class SeleniumTestes(LiveServerTestCase):
         open_browser = True if os.getenv('OPEN_BROWSER') == 'true' else False
 
         if open_browser:
-            browser = webdriver.Chrome()
+            options = webdriver.ChromeOptions()
+            options.add_argument('--start-maximized')
+            options.add_argument('--incognito')
+            browser = webdriver.Chrome(options=options)
         else:
             options = webdriver.ChromeOptions()
+            options.add_argument('--start-maximized')
             options.add_argument('--headless=new')
+            options.add_argument('--incognito')
             browser = webdriver.Chrome(options=options)
 
         browser.get(os.getenv('URL_DEV'))
@@ -47,4 +52,56 @@ class SeleniumTestes(LiveServerTestCase):
         browser.close()
 
         sleep(1)
+    
+    def testDeleteUser(self):
+        # Condig Tests
+        open_browser = True if os.getenv('OPEN_BROWSER') == 'true' else False
+
+        if open_browser:
+            options = webdriver.ChromeOptions()
+            options.add_argument('--start-maximized')
+            options.add_argument('--incognito')
+            browser = webdriver.Chrome(options=options)
+        else:
+            options = webdriver.ChromeOptions()
+            options.add_argument('--start-maximized')
+            options.add_argument('--headless=new')
+            options.add_argument('--incognito')
+            browser = webdriver.Chrome(options=options)
         
+        browser.get(os.getenv('URL_DEV'))
+        actions = ActionChains(browser)
+
+        sleep(3)
+
+
+        buttonListUser = browser.find_element(By.XPATH, "//a[contains(text(), 'Lista de Usuários')]")
+
+        actions.click(buttonListUser).perform()
+
+        sleep(2)
+
+        idUser = browser.find_element(By.XPATH, "(//tr[2]/th)[1]").text
+
+        sleep(2)        
+
+        buttonNavBar = browser.find_element(By.XPATH, "//a[contains(text(), 'Deletar Usuários')]")
+
+        actions.click(buttonNavBar).perform()
+        
+
+        elemId = browser.find_element(By.NAME, 'id')
+
+        Utils.SlowTypes(self, actions, idUser, elemId, 0.001)
+        
+        sleep(3)
+
+        buttonSubmit = browser.find_element(By.XPATH, "//button[contains(text(), 'Apagar')]")
+
+        actions.click(buttonSubmit).perform()
+
+        browser.find_element(By.XPATH, "//div[contains(text(), 'Usuário deletado com sucesso!')]")
+
+        sleep(3)
+
+        browser.close()
